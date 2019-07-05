9034f26b431499e566bf3b2848d30bc50af96b7d
mukeshqas <52527601+mukeshqas@users.noreply.github.com>
2019-07-05 16:41:30 +0530

    Update testingqa.java

--- testingqa.java
+++ testingqa.java
@@ -1,1 +1,10 @@
-public class Add number
+public class Add number{
+ 
+  public static void main(Strings[] args)
+    
+    int num1=5,num2=5,sum;
+    sum=num1+num2;
+  
+  system.out.println("Add these two numbers:"+sum)
+}
+  
